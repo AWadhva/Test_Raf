a1da45ea7cf4eebe8cac6ce3b5f68a8e92ab3596
Anuj <anuj@gmail.com>
2025-06-17 18:08:25 +0530

    ExternalUserClient (http client)

---
+++ ExternalUserService/ExternalUserClient.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Text.Json;
+using ExternalUserService.Models;
+
+public class ExternalUserClient : IExternalUserClient
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ExternalUserClient(IHttpClientFactory httpClientFactory)
+    {
+        if (httpClientFactory == null)
+            throw new ArgumentException("Unexpected: httpClientFactory is null");
+
+        _httpClientFactory = httpClientFactory;
+    }
+
+    private HttpClient CreateClient()
+    {
+        return _httpClientFactory.CreateClient("reqresClient");
+    }
+
+    // TODO: return MayBe
+    public async Task<User> GetUserByIdAsync(int userId)
+    {
+        var client = CreateClient();
+
+        // TODO: handle exception
+        var response = await client.GetAsync($"users/{userId}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        // TODO: handle json exception
+        var parsed = JsonSerializer.Deserialize<UserResponse>(responseBody);
+        return parsed?.Data;
+    }
+
+    // TODO: return MayBe
+    public async Task<List<User>> GetUsersByPageAsync(int page)
+    {
+        var client = CreateClient();
+
+        // TODO: handle exception
+        var response = await client.GetAsync($"users?page={page}");
+
+        response.EnsureSuccessStatusCode();
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        // TODO: handle json exception
+        var parsed = JsonSerializer.Deserialize<UserListResponse>(responseBody);
+        return parsed?.Data ?? new List<User>();
+    }
+}
---
+++ ExternalUserService/IExternalUserClient.cs
@@ -0,0 +1,7 @@
+﻿using ExternalUserService.Models;
+
+public interface IExternalUserClient
+{
+    Task<User> GetUserByIdAsync(int userId);
+    Task<List<User>> GetUsersByPageAsync(int page);
+}
