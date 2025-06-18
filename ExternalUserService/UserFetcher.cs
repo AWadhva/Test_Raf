b0e52e54f29dd2735d6e21677d6be8f99b692f4b
Anuj <anuj@gmail.com>
2025-06-18 09:34:04 +0530

    Extract out FetchAllUsers

--- ExternalUserService/UserFetcher.cs
+++ ExternalUserService/UserFetcher.cs
@@ -12,50 +12,16 @@ public class UserFetcher
     {
         this.fetcher = fetcher;
         this.users = users;
-        
-        Task.Run(async () =>  {
-            var users = await FetchUsers();
-            this.users.SetUsers(users);
+
+        Task.Run((Func<Task?>)(async () =>  {
+            var users = await this.FetchUsers();
+            this.users.SetUsers((Dictionary<int, User>)users);
             await Task.Delay(5 * 60 * 1000); // TODO: assuming data getting stale in 5 minutes
-        });
+        }));
     }
 
     private async Task<Dictionary<int, User>> FetchUsers()
-    {        
-        Dictionary<int , User> users = new();
-
-        int page = 0;
-
-        var userBatch = await fetcher.GetUsersByPageAsync(page);
-        if (userBatch.Users == null || userBatch.TotalPages == null)
-            return users;
-
-        int totalPages = (int)userBatch.TotalPages;
-
-        while (true)
-        {
-            userBatch.Users.ForEach(x =>
-            {
-                if (users.TryGetValue(x.Id, out _))
-                {
-                    // TODO: Duplicate. log message
-                }
-                else
-                    users.Add(x.Id, x);
-            }
-            );
-            
-            page++;
-            if (page == totalPages)
-                break;
-            else
-            {
-                userBatch = await fetcher.GetUsersByPageAsync(page);
-                if (userBatch.Users == null)
-                    break;
-            }
-        }
-
-        return users;
+    {
+        return await UserFetcherHelpers.FetchAllUsers(fetcher);
     }
 }
---
+++ ExternalUserService/UserFetcherHelpers.cs
@@ -0,0 +1,47 @@
+﻿using ExternalUserService.Models;
+
+namespace ExternalUserService
+{
+    internal static class UserFetcherHelpers
+    {
+
+        public static async Task<Dictionary<int, User>> FetchAllUsers(IExternalUserClient fetcher)
+        {
+            Dictionary<int, User> users = new();
+
+            int page = 0;
+
+            var userBatch = await fetcher.GetUsersByPageAsync(page);
+            if (userBatch.Users == null || userBatch.TotalPages == null)
+                return users;
+
+            int totalPages = (int)userBatch.TotalPages;
+
+            while (true)
+            {
+                userBatch.Users.ForEach(x =>
+                {
+                    if (users.TryGetValue(x.Id, out _))
+                    {
+                        // TODO: Duplicate. log message
+                    }
+                    else
+                        users.Add(x.Id, x);
+                }
+                );
+
+                page++;
+                if (page == totalPages)
+                    break;
+                else
+                {
+                    userBatch = await fetcher.GetUsersByPageAsync(page);
+                    if (userBatch.Users == null)
+                        break;
+                }
+            }
+
+            return users;
+        }
+    }
+}
